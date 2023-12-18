46aa6c538749a74a6f0be8a28fdaf6b7927db052
Sajjad Mohammadi <mohammadisajjad54@gmail.com>
2023-12-18 14:20:04 +0330

    Complete home page and header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,19 @@
 <x-master-layout>
-
+    <div class="flex items-center justify-center space-x-5 mt-14">
+        <img class="rounded-xl" src="images/gifs/to-do-list.gif" alt="to-do list gif"/>
+        <div class="flex flex-col items-center space-y-8 justify-center">
+            <h1 class="text-center font-bold text-7xl max-w-3xl">
+                One simple to do list for you and your team
+            </h1>
+            <p class="text-center text-3xl font-semibold leading-[55px] text-gray-600 max-w-[40rem]">
+                Stay focused, organized, and balanced with Todolist.
+                The global No. 1 among apps for to-do lists and task management.
+            </p>
+            <a href="/register" class="w-1/2">
+                <x-buttons.primary class="w-full">
+                    Get Started
+                </x-buttons.primary>
+            </a>
+        </div>
+    </div>
 </x-master-layout>
