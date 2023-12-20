5635f18269a6f04e726ae2499717f1d86d32a899
mohammad ghobadi <mohammadrezaghobadi.1383@gmail.com>
2023-12-20 15:45:11 +0330

    fixed register controller file

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    public function create(){
+        return view('register.create');
+    }
+    public function store(){
+        $atterbutes = request()->validate([
+            'name' => 'required|max:100|min:3',
+            'email' => 'required|email|max:225',
+            'password' => 'required|min:4'
+        ]);
+        User::create($atterbutes);
+        return redirect('login');
+    }
 }
