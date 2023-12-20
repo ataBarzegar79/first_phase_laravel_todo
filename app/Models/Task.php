2058dc612e399e1b56fe4921d89c378843d1c668
mohammad ghobadi <mohammadrezaghobadi.1383@gmail.com>
2023-12-20 15:49:13 +0330

    fixed model task file

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function scopeFilter($query,array $filters): void
+    {
+        $query->when($filters['search'] ?? false,function ($query,$search){
+            $query->where(fn($query) =>
+            $query->where('title' , 'like' , '%' . $search . '%')
+                ->orWhere('slug', 'like' , '%' . $search . '%'));
+        });
+        $query->whereNot(function ($query) {
+            $query
+                ->where('select', '=', 'Completed')
+                ->whereDate('time', '<', Carbon::now()->subDays(7));
+        });
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
